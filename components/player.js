15f60af9964381db72326ccec12c204b93d86007
Scottie Crowe <scottiecrowe@gmail.com>
2023-03-15 10:09:22 -0600

    create player class

--- components/player.js
+++ components/player.js
@@ -1,1 +1,23 @@
 // Create Player class
+
+export class Player {
+    constructor(name) {
+        this.name = name;
+        this.hand = [];
+    }
+
+    // pops card off dealer.deck and pushes to player.hand
+    hit(dealerDeck) {
+        this.hand.push(dealerDeck.cards.pop());
+    }
+
+    // pops card off dealer.deck and pushes card to dealer.hand
+    stay(dealDealer) {
+        dealDealer.hand.push(dealDealer.deck.pop());
+    }
+
+    // call handScorer function to get total value of hand
+    get handTotal() {
+        return handScorer(this.hand);
+    }
+}
