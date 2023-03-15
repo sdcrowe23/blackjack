4a56e83069f57b02e3931268972e29a79c532822
Scottie Crowe <scottiecrowe@gmail.com>
2023-03-15 10:59:54 -0600

    remove scoreHandler function to seperate file

--- components/dealer.js
+++ components/dealer.js
@@ -1,22 +1,5 @@
 // Create Dealer class
-
-const handScorer = (playerHand) => {
-    let total = 0;
-    for (let i = 0; i < playerHand.length; i++) {
-        if (playerHand[i].value === "K" || playerHand[i].value === "Q" || playerHand[i].value === "J") {
-            total += 10;
-        } else if (playerHand[i].value == "A" && total <= 10) {
-            total += 11;
-        } else if (playerHand[i].value == "A" && total >= 11) {
-            total += 1;
-        } else {
-            total += playerHand[i].value;
-        }
-    }
-    return total;
-}
-
-class Dealer {
+export class Dealer {
     constructor() {
         this.name = "Dealer";
         this.deck = new Deck();
