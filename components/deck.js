6d462f4e404575d9b4572e7340da9d52f7ffb29e
Scottie Crowe <scottiecrowe@gmail.com>
2023-03-15 10:05:59 -0600

    create deck class

--- components/deck.js
+++ components/deck.js
@@ -1,1 +1,34 @@
 // Create Deck class
+
+export class Deck {
+    constructor() {
+        this.cards = [];
+        this.initialize();
+        this.shuffle();
+    }
+
+    // immedialy called upon object init
+    initialize() {
+
+        // loop through values/suits of Card object and return Card to this.cards array
+        Object.values(Card.suits).forEach((suit) => {
+            Object.values(Card.values).forEach((value) => {
+                return this.cards.push(new Card(suit, value));
+            })
+        })
+    }
+
+    // shuffles this.card arry after Deck init
+    shuffle() {
+        for (const card_indx in this.cards) {
+            // get random number
+            let randomIndx = Math.floor(Math.random() * (this.cards.length - 1));
+            // get current index of card in this.class
+            let temp = this.cards[card_indx];
+            // swap current index with random index
+            this.cards[card_indx] = this.cards[randomIndx];
+            this.cards[randomIndx] = temp;
+        }
+        return;
+    }
+}
