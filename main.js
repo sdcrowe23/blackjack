769ecf0709c0a550032ad76202a3f632a5d14b5a
Scottie Crowe <scottiecrowe@gmail.com>
2023-03-15 10:32:33 -0600

    outline game play steps

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 // import ES6 Modules
 // Game play happens here
+
+/*
+
+1. Player answers prompt to start game
+2. Two cards are dealt to player and dealer (dealer card(1) is hidden)
+3a. Player has option to 'hit' or 'stay'
+3b. If player chooses to 'hit', card is dealt to player (player hand total value is updated)
+3c. if player chooses to 'stay' then the dealers is dealt another card (dealer hand total value is updated minus hidden card)
+3d. Dealer card is revealed
+4. Dealer hand value is compared with the player hand value
+5. Display win/lose browser console message to player
+6. Ask if the player wants to play again? (repeat steps)
+
+*/
